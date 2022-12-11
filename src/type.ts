fdb30e3f9e5631c26d09d3d587fcaf5fbd7e2ea8
Rafa Puerta <info@rafapuerta.com>
2022-12-11 14:17:17 +0100

    FEAT - Add presence control

--- src/type.ts
+++ src/type.ts
@@ -1,7 +1,8 @@
 export interface Settings {
-  apiToken: string;
-  statusText: string;
-  statusEmoji: string;
-  statusExpiration: "0" | "30m" | "1h" | "4h" | "today" | "week" | "custom";
-  customExpiration: string;
+  apiToken: string
+  statusText: string
+  statusEmoji: string
+  statusExpiration: '0' | '30m' | '1h' | '4h' | 'today' | 'week' | 'custom'
+  customExpiration: string
+  presence: string
 }
